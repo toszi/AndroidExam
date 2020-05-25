830f8260ad6892ba7ed192b86dd320a39e2813b3
Tobias <toszi18@gmail.com>
2020-05-25 16:26:39 +0200

    Added all the entities

---
+++ app/src/main/java/sdu/android/examapp/Entities/Clouds.java
@@ -0,0 +1,13 @@
+package sdu.android.examapp.Entities;
+
+public class Clouds {
+    private int all;
+
+    public Clouds(int all) {
+        this.all = all;
+    }
+
+    public int getAll() {
+        return all;
+    }
+}
---
+++ app/src/main/java/sdu/android/examapp/Entities/CompleteWeatherInfo.java
@@ -0,0 +1,89 @@
+package sdu.android.examapp.Entities;
+
+import java.util.ArrayList;
+
+public class CompleteWeatherInfo {
+    private Coord coord;
+    private ArrayList<Weather> weather;
+    private String base;
+    private Main main;
+    private int visibility;
+    private Wind wind;
+    private Clouds clouds;
+    private int dt;
+    private Sys sys;
+    private int timezone;
+    private int id;
+    private String name;
+    private int cod;
+
+    public CompleteWeatherInfo(Coord coord, ArrayList<Weather> weather, String base,
+                               Main main, int visibility, Wind wind, Clouds clouds, int dt,
+                               Sys sys, int timezone, int id, String name, int cod) {
+        this.coord = coord;
+        this.weather = weather;
+        this.base = base;
+        this.main = main;
+        this.visibility = visibility;
+        this.wind = wind;
+        this.clouds = clouds;
+        this.dt = dt;
+        this.sys = sys;
+        this.timezone = timezone;
+        this.id = id;
+        this.name = name;
+        this.cod = cod;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public ArrayList<Weather> getWeather() {
+        return weather;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+
+    public int getDt() {
+        return dt;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+    public int getTimezone() {
+        return timezone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+}
